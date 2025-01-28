c2ee1fd840108b1955c9f90b1891e300729f05ee
raphael-devasia <jinomaliyekkal@Jinos-MacBook-Air.local>
2025-01-28 10:37:42 +0530

    final commit

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:3000/api',
+  // backendUrl: 'http://localhost:3000/api',
+  backendUrl: 'https://cms-backend-1-jofv.onrender.com/api',
 };
